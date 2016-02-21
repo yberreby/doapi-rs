10b47a84af805c282099d65017d3765d8e589851
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2016-02-21 11:29:02 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,34 @@
+/// A DigitalOcean client.
 pub struct Client {
     token: String,
 }
 
 impl Client {
+    /// Create a new Client with the given API token.
     pub fn with_token<T: Into<String>>(tok: T) -> Client {
         Client { token: tok.into() }
     }
+
+    pub fn droplets(&self) -> DropletsService {
+        DropletsService::with_token(&self.token)
+    }
 }
+
+
+
+pub struct DropletsService<'tok> {
+    token: &'tok str,
+}
+
+impl<'tok> DropletsService<'tok> {
+    pub fn with_token(token: &str) -> DropletsService {
+        DropletsService { token: token }
+    }
+
+    pub fn create(params: &DropletCreateParams) -> Droplet {
+        unimplemented!()
+    }
+}
+
+pub struct Droplet;
+pub struct DropletCreateParams;
